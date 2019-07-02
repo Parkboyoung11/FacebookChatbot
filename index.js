5abb297fa554831140d3602ae331d89e2a8d5a23
aki <aki@akis-MacBook-Pro.local>
2019-07-02 08:15:46 +0700

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+var login = require("facebook-chat-api");
+ 
+var answeredThreads = {};
+ 
+// Create simple echo bot
+login({email: "", password: ""}, function callback (err, api) {
+    if(err) return console.error(err);
+ 
+    api.listen(function callback(err, message) {
+        console.log(message.threadID);
+        // if(!answeredThreads.hasOwnProperty(message.threadID)){
+            answeredThreads[message.threadID] = true;
+            api.sendMessage("Em chào các anh ạ :v", message.threadID);
+        // }
+    });
+});
